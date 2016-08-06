4bff432ebbecef4ea17cbfa1562524adca5a46ad
Jon Chui <jon@lpcuwc.edu.hk>
2016-08-07 00:18:45 +0800

    Initial commit

---
+++ app/components/Weather.jsx
@@ -0,0 +1,53 @@
+var React = require('react');
+var WeatherForm = require('WeatherForm');
+var WeatherMessage = require('WeatherMessage');
+var openWeatherMap = require('openWeatherMap');
+
+var Weather = React.createClass({
+  getInitialState: function () {
+    return {
+      isLoading: false
+    }
+  },
+  handleSearch: function (location) {
+
+    this.setState({isLoading:true});
+
+    openWeatherMap.getTemp(location).then(
+      (temp) => {
+                  this.setState({
+                    isLoading: false,
+                    location: location,
+                    temp: temp
+                  });
+                },
+      (errorMessage) => {
+                  this.setState({
+                    isLoading: false
+                  });
+                  alert(errorMessage);
+                }
+      );
+  },
+  render: function () {
+    var {isLoading, temp, location} = this.state;
+
+    function renderMessage () {
+      if (isLoading) {
+        return <h3>fetching weather</h3>
+      } else if (temp && location) {
+        return <WeatherMessage temp={temp} location={location}/>;
+      }
+    }
+
+    return (
+      <div>
+        <h3>Weather Component</h3>
+        <WeatherForm onSearch={this.handleSearch}/>
+        {renderMessage()}
+      </div>
+    )
+  }
+});
+
+module.exports = Weather;

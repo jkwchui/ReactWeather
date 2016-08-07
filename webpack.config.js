fa0a842d98ff787fd3aa83dea95853082e7dd532
Jon Chui <jon@lpcuwc.edu.hk>
2016-08-07 12:49:20 +0800

    Add foundation & jQuery

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,21 @@
+var webpack = require('webpack');
+
 module.exports = {
-  entry: './app/app.jsx',
+  entry: [
+    'script!jquery/dist/jquery.min.js',
+    'script!foundation-sites/dist/foundation.min.js',
+    './app/app.jsx'
+
+  ],
+  externals: {
+    jquery: 'jQuery'
+  },
+  plugins: [
+    new webpack.ProvidePlugin({
+      '$': 'jquery',
+      'jQuery': 'jquery'
+    })
+  ],
   output: {
     path: __dirname,
     filename: './public/bundle.js'
@@ -20,16 +36,14 @@ module.exports = {
     extensions: ['', '.js', '.jsx']
   },
   module: {
-    loaders: [
-      {
-        loader: 'babel-loader',
-        query: {
-          presets: ['react', 'es2015', 'stage-0']
-        },
-        test: /\.jsx?$/,
-        exclude: /(node_modules|bower_components)/
-      }
-    ]
+    loaders: [{
+      loader: 'babel-loader',
+      query: {
+        presets: ['react', 'es2015', 'stage-0']
+      },
+      test: /\.jsx?$/,
+      exclude: /(node_modules|bower_components)/
+    }]
   },
   devtool: 'cheap-module-eval-source-map'
 };
